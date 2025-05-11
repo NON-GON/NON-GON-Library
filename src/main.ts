f4548b14e80ebea243fa59f05a939a9134888445
loumourao <71392356+loumourao@users.noreply.github.com>
2025-05-11 01:39:40 +0100

    Modify webpage and add specific scenes
    Developed a landing page with consistent designs (yet to be further refined) and started building specific scenes for each shape type and interaction (yet to be further refined).

--- src/main.ts
+++ src/main.ts
@@ -2,469 +2,54 @@ import * as THREE from "three";
 import { GeometryManager } from "./Geometries/GeometryManager";
 import { GeometryType2D, GeometryType3D } from "./Geometries/GeoTypes";
 import { Vector3, Vector2 } from "./Calc/Util/Utils";
-import { SVGRenderer } from "three/examples/jsm/renderers/SVGRenderer";
+import { BoxLineGeometry } from "three/examples/jsm/geometries/BoxLineGeometry";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { Point2DScene, LineSegment2DScene, Circle2DScene,
+         Ellipse2DScene, Superellipse2DScene, GeneralSmoothConvexShape2DScene,
+         Point3DScene, Plane3DScene, Ellipsoid3DScene,
+         Superellipsoid3DScene, Cylinder3DScene, Hyperboloid3DScene,
+         EllipticParaboloid3DScene, StrictlyConvexShape3DScene } from "./scenes";
 
-const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xfffafa);
-let geometryManager = new GeometryManager();
+// Color Palette
+const RED = 0xA32545;
+const GREEN = 0x5D803D;
+const BLUE = 0x6BA7C7;
+const BURNT_YELLOW = 0xCE7E0C;
+const PURPLE = 0xCC7ECC;
+const DARK_BACKGROUND = 0x2F3336;
 
-let angle = 0; // Add a variable to track the rotation angle
-let hex = 0x00ff00;
-let hexBlue = 0xb0c4de;
-let hexRed = 0xf08080;
+const scenes: Record<string, new (canvas: HTMLCanvasElement) => any> = {
+  'Point2D': Point2DScene,
+  'LineSegment2D': LineSegment2DScene,
+  'Circle2D': Circle2DScene,
+  'Ellipse2D': Ellipse2DScene,
+  'Superellipse2D': Superellipse2DScene,
+  'GeneralSmoothConvexShape2D': GeneralSmoothConvexShape2DScene,
+  'Point3D': Point3DScene,
+  'Plane3D': Plane3DScene,
+  'Ellipsoid3D': Ellipsoid3DScene,
+  'Superellipsoid3D': Superellipsoid3DScene,
+  'Cylinder3D': Cylinder3DScene,
+  'Hyperboloid3D': Hyperboloid3DScene,
+  'EllipticParaboloid3D': EllipticParaboloid3DScene,
+  'StrictlyConvexShape3D': StrictlyConvexShape3DScene
+};
 
-initialization();
-function initialization() {
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
-  const renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth - 15, window.innerHeight - 20);
-  document.body.appendChild(renderer.domElement);
-  camera.position.z = 50;
+const canvas = document.getElementById('c') as HTMLCanvasElement;
 
-  const svgRenderer = new SVGRenderer(); // Create an SVG renderer
-  svgRenderer.setSize(window.innerWidth - 15, window.innerHeight - 20);
-  document.body.appendChild(svgRenderer.domElement); // Append SVG renderer to the DOM
+document.querySelectorAll<HTMLAnchorElement>('.gallery-time').forEach(item => {
+  item.addEventListener('click', e => {
+    e.preventDefault();
 
-  scene.userData.renderer = renderer; // Store the renderer in scene's userData
-  scene.userData.svgRenderer = svgRenderer; // Store the SVG renderer in scene's userData
-  scene.userData.camera = camera; // Store the camera in scene's userData
+    const key = item.dataset.type;
+    const SceneType = scenes[key];
 
-  document.addEventListener("keydown", (event) => {
-    if (event.key.toLowerCase() === "p") {
-      exportSceneToSVG(); // Call the export function when "P" is pressed
+    if (!SceneType) {
+      console.warn('No Scene registered for "${key}"');
+      return;
+    } else {
+      const scene = new SceneType(canvas);
+      scene.start()
     }
   });
-
-  animate(renderer, scene, camera);
-}
-
-function animate(renderer: any, scene: any, camera: any) {
-  requestAnimationFrame(() => animate(renderer, scene, camera));
-
-  // Update the camera's position to rotate around the scene
-  angle += 0.01; // Adjust the speed of rotation
-  camera.position.x = 50 * Math.cos(angle);
-  camera.position.z = 50 * Math.sin(angle);
-  camera.lookAt(0, 0, 0); // Ensure the camera always looks at the center of the scene
-
-  renderer.render(scene, camera);
-}
-
-function exportSceneToSVG() {
-  const svgRenderer = scene.userData.svgRenderer;
-  const camera = scene.userData.camera; // Retrieve the camera from userData
-  if (!svgRenderer || !camera) {
-    console.error("SVGRenderer or Camera is not initialized.");
-    return;
-  }
-
-  svgRenderer.render(scene, camera); // Render the scene to SVG using the correct camera
-  const svgElement = svgRenderer.domElement;
-  const svgData = new XMLSerializer().serializeToString(svgElement);
-
-  const blob = new Blob([svgData], { type: "image/svg+xml" });
-  const url = URL.createObjectURL(blob);
-
-  const link = document.createElement("a");
-  link.href = url;
-  link.download = "scene.svg";
-  link.click();
-
-  URL.revokeObjectURL(url); // Clean up the URL object
-}
-
-function pointEllipseMDTest() {
-  let params0 = {
-    center: new Vector2(20, 0),
-  };
-  geometryManager.createGeometry(GeometryType2D.Point, "geo0", params0);
-
-  let params1 = {
-    center: new Vector2(0, 0),
-    xradius: 10,
-    yradius: 5,
-    rotation: new Vector3(0, 0, 0),
-    segments: 100,
-  };
-  geometryManager.createGeometry(GeometryType2D.Ellipse, "geo1", params1);
-
-  scene.add(geometryManager.getGeometryMesh("geo1", hexBlue));
-  scene.add(geometryManager.getGeometryMesh("geo0", hexRed));
-
-  let points = geometryManager.calculateMinimumDistance("geo0", "geo1");
-  drawMinimumDistance(points[0], points[1]);
-}
-
-function ellipseEllipseMDTest() {
-  let params0 = {
-    center: new Vector2(0, 2),
-    xradius: 5,
-    yradius: 2,
-    rotation: new Vector3(0, 0, 0),
-    segments: 100,
-  };
-  geometryManager.createGeometry(GeometryType2D.Ellipse, "geo0", params0);
-
-  let params1 = {
-    center: new Vector2(0, 0),
-    xradius: 4,
-    yradius: 2,
-    rotation: new Vector3(0, 0, 0),
-    segments: 100,
-  };
-  geometryManager.createGeometry(GeometryType2D.Ellipse, "geo1", params1);
-
-  scene.add(geometryManager.getGeometryMesh("geo1", hexBlue));
-  scene.add(geometryManager.getGeometryMesh("geo0", hexRed));
-
-  let points = geometryManager.calculateMinimumDistance("geo1", "geo0");
-  drawMinimumDistance(points[0], points[1]);
-}
-
-function superellipseLineMDTest() {
-  let params0 = {
-    center: new Vector2(0, 0),
-    xradius: 5,
-    yradius: 5,
-    exponent: 2,
-    rotation: new Vector3(0, 0, 0),
-    segments: 100,
-  };
-  let params1 = {
-    start: new Vector3(0, 5, 0),
-    end: new Vector3(0, 7, 0),
-    rotation: new Vector3(0, 0, 0),
-  };
-  geometryManager.createGeometry(GeometryType2D.Supperellipse, "geo0", params0);
-  geometryManager.createGeometry(GeometryType2D.Line, "geo1", params1);
-  scene.add(geometryManager.getGeometryMesh("geo1", hexBlue));
-  scene.add(geometryManager.getGeometryMesh("geo0", hexRed));
-  let points = geometryManager.calculateMinimumDistance("geo0", "geo1");
-
-  drawMinimumDistance(points[0], points[1]);
-}
-
-function pointEllipsoidMDTest() {
-  let params0 = {
-    center: new Vector2(0, 0),
-  };
-  geometryManager.createGeometry(GeometryType2D.Point, "geo0", params0);
-  let params1 = {
-    center: new Vector3(15, 0, 0),
-    xradius: 10,
-    yradius: 5,
-    zradius: 5,
-    segments: 100,
-  };
-  geometryManager.createGeometry(GeometryType3D.Ellipsoid, "geo1", params1);
-  scene.add(geometryManager.getGeometryMesh("geo1", hexBlue));
-  scene.add(geometryManager.getGeometryMesh("geo0", hexRed));
-  let points = geometryManager.calculateMinimumDistance("geo0", "geo1");
-  drawMinimumDistance(points[0], points[1]);
-}
-
-function EllipsoidEllipsoidMDTest() {
-  let geometryManager = new GeometryManager();
-  let params0 = {
-    center: new Vector3(-10, 15, 5),
-    xradius: 4,
-    yradius: 12,
-    zradius: 8,
-    rotation: new Vector3(0, 0, 0),
-    segments: 15,
-  };
-  geometryManager.createGeometry(GeometryType3D.Ellipsoid, "geo0", params0);
-  let params1 = {
-    center: new Vector3(15, 6, 0),
-    xradius: 7,
-    yradius: 5,
-    zradius: 3,
-    rotation: new Vector3(0, 0, 0),
-    segments: 15,
-  };
-  geometryManager.createGeometry(GeometryType3D.Ellipsoid, "geo1", params1);
-  scene.add(geometryManager.getGeometryMesh("geo1", hexRed));
-  scene.add(geometryManager.getGeometryMesh("geo0", hexBlue));
-  let points = geometryManager.calculateMinimumDistance("geo0", "geo1");
-  drawMinimumDistance(points[0], points[1]);
-}
-
-function SuperellipsoidPlaneMDTest() {
-  let params0 = {
-    center: new Vector3(0, 20, 0),
-    xradius: 5,
-    yradius: 5,
-    zradius: 5,
-    segments: 100,
-    e1: 5,
-    e2: 5,
-  };
-  let params1 = {
-    center: new Vector2(0, 0),
-    segments: 100,
-    rotation: 0,
-    width: 10,
-    height: 10,
-  };
-  geometryManager.createGeometry(
-    GeometryType3D.Superellipsoid,
-    "geo0",
-    params0
-  );
-  geometryManager.createGeometry(GeometryType2D.Plane, "geo1", params1);
-  scene.add(geometryManager.getGeometryMesh("geo1", hexRed));
-  scene.add(geometryManager.getGeometryMesh("geo0", hexBlue));
-  let points = geometryManager.calculateMinimumDistance("geo0", "geo1");
-  drawMinimumDistance(points[0], points[1]);
-}
-
-function ellipseEllipsePQTest() {
-  const paramsEllipse0 = {
-    center: new Vector2(0, 8),
-    xradius: 5,
-    yradius: 2,
-    rotation: new Vector3(0, 0, 0),
-    segments: 100,
-  };
-  const paramsEllipse1 = {
-    center: new Vector2(0, 0),
-    xradius: 4,
-    yradius: 2,
-    rotation: new Vector3(0, 0, 0),
-    segments: 100,
-  };
-  geometryManager.createGeometry(
-    GeometryType2D.Ellipse,
-    "geo0",
-    paramsEllipse0
-  );
-  geometryManager.createGeometry(
-    GeometryType2D.Ellipse,
-    "geo1",
-    paramsEllipse1
-  );
-  scene.add(geometryManager.getGeometryMesh("geo1", hexRed));
-  scene.add(geometryManager.getGeometryMesh("geo0", hexBlue));
-  const resultAlberich = geometryManager.calculateProximityQuery(
-    "geo0",
-    "geo1",
-    "Alberich"
-  );
-  const resultCaravantes = geometryManager.calculateProximityQuery(
-    "geo0",
-    "geo1",
-    "Caravantes"
-  );
-  console.log("Alberich Result:", resultAlberich);
-  console.log("Bishop Result:", resultCaravantes);
-}
-
-function cylinderCylinderPQTest() {
-  const paramsCylinder0 = {
-    center: new Vector3(0, 5, 0),
-    xradius: 5,
-    yradius: 5,
-    height: 10,
-    rotation: new Vector3(0, 0, 0),
-    segments: 100,
-  };
-  const paramsCylinder1 = {
-    center: new Vector3(0, 0, 0),
-    xradius: 5,
-    yradius: 5,
-    height: 10,
-    rotation: new Vector3(0, 0, 0),
-    segments: 100,
-  };
-  geometryManager.createGeometry(
-    GeometryType3D.Cylinder,
-    "geo0",
-    paramsCylinder0
-  );
-  geometryManager.createGeometry(
-    GeometryType3D.Cylinder,
-    "geo1",
-    paramsCylinder1
-  );
-  scene.add(geometryManager.getGeometryMesh("geo1", hexRed));
-  scene.add(geometryManager.getGeometryMesh("geo0", hexBlue));
-  const resultChittawadigi = geometryManager.calculateProximityQuery(
-    "geo0",
-    "geo1",
-    "Chittawadigi"
-  );
-  console.log("Chittawadigi Result:", resultChittawadigi);
-}
-
-function ellipsoidEllipsoidPQTest() {
-  const paramsEllipsoid0 = {
-    center: new Vector3(0, 5, 0),
-    xradius: 5,
-    yradius: 5,
-    zradius: 5,
-    rotation: new Vector3(0, 0, 0),
-    segments: 20,
-  };
-  const paramsEllipsoid1 = {
-    center: new Vector3(0, 0, 0),
-    xradius: 5,
-    yradius: 5,
-    zradius: 5,
-    rotation: new Vector3(0, 0, 0),
-    segments: 20,
-  };
-  geometryManager.createGeometry(
-    GeometryType3D.Ellipsoid,
-    "geo0",
-    paramsEllipsoid0
-  );
-  geometryManager.createGeometry(
-    GeometryType3D.Ellipsoid,
-    "geo1",
-    paramsEllipsoid1
-  );
-  scene.add(geometryManager.getGeometryMesh("geo1", hexRed));
-  scene.add(geometryManager.getGeometryMesh("geo0", hexBlue));
-  const resultCaravantes = geometryManager.calculateProximityQuery(
-    "geo0",
-    "geo1",
-    "Caravantes"
-  );
-  console.log("Caravantes Result:", resultCaravantes);
-}
-
-function ellipsoidEllipticParaboloidPQTest() {
-  const paramsEllipsoid = {
-    center: new Vector3(0, 5, 0),
-    xradius: 5,
-    yradius: 5,
-    zradius: 5,
-    rotation: new Vector3(0, 0, 0),
-    segments: 15,
-  };
-  const paramsEllipticParaboloid = {
-    center: new Vector3(0, 0, 0),
-    xradius: 5,
-    yradius: 5,
-    height: 10,
-    rotation: new Vector3(0, 0, 0),
-    segments: 15,
-  };
-  geometryManager.createGeometry(
-    GeometryType3D.Ellipsoid,
-    "geo0",
-    paramsEllipsoid
-  );
-  geometryManager.createGeometry(
-    GeometryType3D.EllipticParaboloid,
-    "geo1",
-    paramsEllipticParaboloid
-  );
-  scene.add(geometryManager.getGeometryMesh("geo1", hexRed));
-  scene.add(geometryManager.getGeometryMesh("geo0", hexBlue));
-  const resultCaravantes = geometryManager.calculateProximityQuery(
-    "geo0",
-    "geo1"
-  );
-  console.log("Caravantes Result:", resultCaravantes);
-}
-
-function almostConvexGeometryPlaneMDTest() {
-  const paramsConvex = {
-    center: new Vector3(0, 10, 0),
-    rotation: new Vector3(0, 0, 0),
-    segments: 100,
-  };
-  const paramsPlane = {
-    center: new Vector3(0, 0, 0),
-    rotation: new Vector3(0, 0, 0),
-    width: 10,
-    height: 10,
-    segments: 100,
-  };
-  geometryManager.createGeometry(GeometryType3D.Convex, "geo0", paramsConvex);
-  geometryManager.createGeometry(GeometryType2D.Plane, "geo1", paramsPlane);
-  scene.add(geometryManager.getGeometryMesh("geo1", hexRed));
-  scene.add(geometryManager.getGeometryMesh("geo0", hexBlue));
-  const points = geometryManager.calculateMinimumDistance("geo1", "geo0");
-  drawMinimumDistance(points[0], points[1]);
-}
-
-function convexLineLineMDTest() {
-  const paramsConvexLine = {
-    center: new Vector3(40, 0, 0),
-    rotation: new Vector3(0, 0, 0),
-    segments: 200,
-  };
-  const paramsLine = {
-    start: new Vector3(-10, 0, 0),
-    end: new Vector3(100, 0, 0),
-    rotation: new Vector3(0, 0, 0),
-  };
-
-  geometryManager.createGeometry(
-    GeometryType2D.ConvexLine,
-    "geo0",
-    paramsConvexLine
-  );
-
-  geometryManager.createGeometry(GeometryType2D.Line, "geo1", paramsLine);
-  scene.add(geometryManager.getGeometryMesh("geo1", hexRed));
-  scene.add(geometryManager.getGeometryMesh("geo0", hexBlue));
-  const points = geometryManager.calculateMinimumDistance("geo1", "geo0");
-  drawMinimumDistance(points[0], points[1]);
-}
-
-function convexCircleCircleMD() {
-  const paramsConvexCircle = {
-    center: new Vector3(-15, 10, 0),
-    radius: 5,
-    rotation: new Vector3(0, 0, 0),
-    segments: 100,
-  };
-  const paramsCircle = {
-    center: new Vector3(0, 0, 0),
-    radius: 5,
-    rotation: new Vector3(0, 0, 0),
-    segments: 100,
-  };
-  geometryManager.createGeometry(
-    GeometryType2D.ConvexCircle,
-    "geo0",
-    paramsConvexCircle
-  );
-  geometryManager.createGeometry(GeometryType2D.Circle, "geo1", paramsCircle);
-  scene.add(geometryManager.getGeometryMesh("geo1", hexRed));
-  scene.add(geometryManager.getGeometryMesh("geo0", hexBlue));
-  const points = geometryManager.calculateMinimumDistance("geo0", "geo1");
-  drawMinimumDistance(points[0], points[1]);
-}
-
-function drawMinimumDistance(
-  point1: Vector3 | Vector2,
-  point2: Vector3 | Vector2
-) {
-  const lineMaterial = new THREE.LineBasicMaterial({ color: 0x080808 });
-  const lineGeometry = new THREE.BufferGeometry().setFromPoints([
-    new THREE.Vector3(
-      parseFloat(point1.x.toFixed(3)),
-      parseFloat(point1.y.toFixed(3)),
-      "z" in point1 ? parseFloat((point1 as Vector3).z.toFixed(3)) : 0
-    ),
-    new THREE.Vector3(
-      parseFloat(point2.x.toFixed(3)),
-      parseFloat(point2.y.toFixed(3)),
-      "z" in point2 ? parseFloat((point2 as Vector3).z.toFixed(3)) : 0
-    ),
-  ]);
-
-  const line = new THREE.Line(lineGeometry, lineMaterial);
-  scene.add(line);
-}
-
-EllipsoidEllipsoidMDTest();
+});
---
+++ src/scenes/Superellipsoid3DScene.ts
@@ -0,0 +1,27 @@
+import * as THREE from 'three';
+import { BaseScene } from './BaseScene';
+
+export class Superellipsoid3DScene extends BaseScene {
+
+  protected buildScene(): void {
+    const radius = 2;
+    const widthSegments = 16;
+    const heightSegments = 16;
+    const geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
+    const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    const pointMesh = new THREE.Mesh(geometry, material);
+
+    this.scene.add(pointMesh);
+  }
+
+  public start(): void {
+    this.buildScene();
+    this.renderLoop();
+  }
+
+  private renderLoop = (): void => {
+    this.controls.update();
+    this.render();
+    requestAnimationFrame(this.renderLoop);
+  }
+}
