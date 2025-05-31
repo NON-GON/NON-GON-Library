45923c493e5493b3e0616b5c78faf5d2861cb34e
loumourao <71392356+loumourao@users.noreply.github.com>
2025-05-31 19:35:46 +0100

    Add all 3D proximity queries

---
+++ src/scenes/CylinderCylinderPQScene.ts
@@ -0,0 +1,58 @@
+import { Cylinder3D } from "./shapes/3D/Cylinder3D";
+import { CylinderCylinderPQ } from "./proximity_queries/3D/CylinderCylinderPQ";
+import { Vector3 } from "../Calc/Util/Utils";
+import { Colors } from "../colors";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const canvas = document.getElementById("c") as HTMLCanvasElement;
+
+  // Cylinder1
+  const cylinder1Center = new Vector3(-50, 50, 50);
+  const cylinder1XRadius = 25;
+  const cylinder1YRadius = 25;
+  const cylinder1Height = 100;
+  const cylinder1Rotation = new Vector3(0, 0, 0);
+  const cylinder1Segments = 100;
+  const cylinder1Id = "Blue Cylinder";
+  const cylinder1Color = Colors.SHAPE1_COLOR;
+  const cylinder1 = new Cylinder3D(
+    canvas,
+    cylinder1Center,
+    cylinder1XRadius,
+    cylinder1YRadius,
+    cylinder1Height,
+    cylinder1Rotation,
+    cylinder1Segments,
+    cylinder1Id,
+    cylinder1Color
+  );
+
+  // Cylinder2
+  const cylinder2Center = new Vector3(50, -50, -50);
+  const cylinder2XRadius = 25;
+  const cylinder2YRadius = 25;
+  const cylinder2Height = 100;
+  const cylinder2Rotation = new Vector3(0, 0, 0);
+  const cylinder2Segments = 100;
+  const cylinder2Id = "Red Cylinder";
+  const cylinder2Color = Colors.SHAPE2_COLOR;
+  const cylinder2 = new Cylinder3D(
+    canvas,
+    cylinder2Center,
+    cylinder2XRadius,
+    cylinder2YRadius,
+    cylinder2Height,
+    cylinder2Rotation,
+    cylinder2Segments,
+    cylinder2Id,
+    cylinder2Color
+  );
+
+  // Interaction
+  const cylinderCylinder = new CylinderCylinderPQ(
+    canvas,
+    cylinder1,
+    cylinder2
+  );
+  cylinderCylinder.startAnimation();
+});
---
+++ src/scenes/HyperboloidPlanePQScene.ts
@@ -0,0 +1,59 @@
+import { Hyperboloid3D } from "./shapes/3D/Hyperboloid3D";
+import { Plane3D } from "./shapes/3D/Plane3D";
+import { HyperboloidPlanePQ } from "./proximity_queries/3D/HyperboloidPlanePQ";
+import { Vector3 } from "../Calc/Util/Utils";
+import { Colors } from "../colors";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const canvas = document.getElementById("c") as HTMLCanvasElement;
+
+  // Hyperboloid
+  const hyperboloidCenter = new Vector3(-50, 50, 50);
+  const hyperboloidXRadius = 12.5;
+  const hyperboloidYRadius = 12.5;
+  const hyperboloidZFactor = 12;
+  const hyperboloidHeight = 75;
+  const hyperboloidRotation = new Vector3(90, 0, 0);
+  const hyperboloidSegments = 100;
+  const hyperboloidId = "Hyperboloid";
+  const hyperboloidColor = Colors.SHAPE1_COLOR;
+  const hyperboloid = new Hyperboloid3D(
+    canvas,
+    hyperboloidCenter,
+    hyperboloidXRadius,
+    hyperboloidYRadius,
+    hyperboloidZFactor,
+    hyperboloidHeight,
+    hyperboloidRotation,
+    hyperboloidSegments,
+    hyperboloidId,
+    hyperboloidColor
+  );
+
+  // Plane
+  const planeCenter = new Vector3(50, -50, -50);
+  const planeRotation = new Vector3(0, 0, 90);
+  const planeWidth = 50;
+  const planeHeight = 100;
+  const planeSegments = 100;
+  const planeId = "Plane";
+  const planeColor = Colors.SHAPE2_COLOR;
+  const plane = new Plane3D(
+    canvas,
+    planeCenter,
+    planeRotation,
+    planeWidth,
+    planeHeight,
+    planeSegments,
+    planeId,
+    planeColor
+  );
+
+  // Interaction
+  const hyperboloidPlane = new HyperboloidPlanePQ(
+    canvas,
+    hyperboloid,
+    plane
+  );
+  hyperboloidPlane.startAnimation();
+});
